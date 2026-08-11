96894c796d2180d57c05d457c94d59d7dc87f8bc
t1an <1402772884@qq.com>
2026-08-11 16:44:47 +0800

    fix: 修复 RabbitMQ 容器启动参数

--- deploy/rabbitmq-entrypoint.sh
+++ deploy/rabbitmq-entrypoint.sh
@@ -4,4 +4,8 @@ set -eu
 RABBITMQ_DEFAULT_PASS="$(cat /run/ai-workflow-secrets/rabbitmq_password)"
 export RABBITMQ_DEFAULT_PASS
 
+if [ "$#" -eq 0 ]; then
+  set -- rabbitmq-server
+fi
+
 exec docker-entrypoint.sh "$@"
